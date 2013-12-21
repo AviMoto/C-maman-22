768e5bc593156618f7f75afc501c29fa263e5ed3
Avi <Avi@admintheweb.com>
2013-12-22 01:05:41 +0200

    First Commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Avi Levy
+ Version     : 1.0.0
+ Copyright   : All Right for Avi Levy
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
